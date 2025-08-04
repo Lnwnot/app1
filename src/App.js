cb044a5d7fc29897d2132cd78eb5b3bc1ac5418b
Lnwnot <23008act@gmail.com>
2025-08-04 10:33:20 +0700

    calculator

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import Button from './class-components'
+import {Calculator} from './calculator'
 
 function App(){
-  return <Button/>
+  return <Calculator/>
 }
 export default App;
